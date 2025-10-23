5ea5d24ee7cea779f15671ea6adf60d888d86144
ananyapant663 <ananyapant663@gmail.com>
2025-10-23 10:45:35 +0530

    Print all sub-strings of a string. Input 1: abc Output 1: a,ab,abc,b,bc,c

---
+++ q100.c
@@ -0,0 +1,29 @@
+/*Print all sub-strings of a string.
+Input 1:
+abc
+Output 1:
+a,ab,abc,b,bc,c*/
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = 0;
+    while(str[len] != '\0' && str[len] != '\n'){
+        len++;
+    }
+
+    for(int i=0; i<len; i++){
+        for(int j=i; j<len; j++){
+            for(int k=i; k<=j; k++){
+                printf("%c", str[k]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
